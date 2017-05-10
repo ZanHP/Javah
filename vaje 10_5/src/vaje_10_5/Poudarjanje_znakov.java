41e3ddaca949a0c0247be2080135e5f57c4ef366
Zan Hafner Petrovski <zanuha007@gmail.com>
2017-05-10 13:59:35 +0200

    vaje 10.5.

--- vaje 10_5/src/vaje_10_5/Poudarjanje_znakov.java
+++ vaje 10_5/src/vaje_10_5/Poudarjanje_znakov.java
@@ -3,8 +3,33 @@ package vaje_10_5;
 public class Poudarjanje_znakov {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//v_veliko("bjjjjlablo");
+		ozn_v_veliko("Zadnja *novica* danes!");
+	}
+	
+	public static void v_veliko(String niz) {
+		for (int index = 0; index < niz.length(); index++) {
+			char crka = niz.charAt(index);
+			System.out.print(Character.toUpperCase(crka) + " ");
+		}
+	}
+	
+	public static void ozn_v_veliko(String niz) {
+		boolean vecati = false;
+		for (int index = 0; index < niz.length(); index++) {
+			if (niz.charAt(index) == '*') {
+				vecati = !vecati;
+			}
+			if (!vecati) {
+				char crka1 = niz.charAt(index);
+				System.out.print(crka1 + " ");
+			}
+			if (vecati) {
+				char crka2 = niz.charAt(index);
+				System.out.print(Character.toUpperCase(crka2) + " ");
+			}
 
+		}
 	}
 
 }
